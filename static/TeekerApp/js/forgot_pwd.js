c85d6949362388918b2cb3e2b776bca46e67bf65
Sana Nqoba Mngadi <sana.mngadi@gmail.com>
2020-01-13 09:44:36 +0200

    Javascript now functional on Register page and forgot password.

--- static/TeekerApp/js/forgot_pwd.js
+++ static/TeekerApp/js/forgot_pwd.js
@@ -5,20 +5,20 @@ document.addEventListener("DOMContentLoaded", () => {
 	// Used to check password and confirm password
     document.querySelector("#pwd").onchange = () => {
         if (document.querySelector("#pwd").value.length > 7 && document.querySelector("#pwd").value.length < 65) {
-            document.querySelector("#pwd").style.borderColor = "green"; // Turn the field borders color green
+            document.querySelector("#u1306-4").style.borderColor = "green"; // Turn the field borders color green
         } else {
-            document.querySelector("#pwd").style.borderColor = "red"; // Turn the field borders color red
+            document.querySelector("#u1306-4").style.borderColor = "red"; // Turn the field borders color red
         }
     }
     document.querySelector("#cpwd").onchange = () => {
         if (document.querySelector("#cpwd").value.length > 7 && document.querySelector("#cpwd").value.length < 65 &&
         document.querySelector("#pwd").value.length > 7 && document.querySelector("#pwd").value.length < 65 &&
         document.querySelector("#pwd").value === document.querySelector("#cpwd").value) {
-            document.querySelector("#pwd").style.borderColor = "green"; // Turn the field borders color green
-            document.querySelector("#cpwd").style.borderColor = "green"; // Turn the field borders color green
+            document.querySelector("#u1306-4").style.borderColor = "green"; // Turn the field borders color green
+            document.querySelector("#u1330-4").style.borderColor = "green"; // Turn the field borders color green
         } else {
-            document.querySelector("#pwd").style.borderColor = "red"; // Turn the field borders color red
-            document.querySelector("#cpwd").style.borderColor = "red"; // Turn the field borders color red
+            document.querySelector("#u1306-4").style.borderColor = "red"; // Turn the field borders color red
+            document.querySelector("#u1330-4").style.borderColor = "red"; // Turn the field borders color red
         }
     }
 });
--- static/TeekerApp/js/register.js
+++ static/TeekerApp/js/register.js
@@ -14,9 +14,9 @@ document.addEventListener("DOMContentLoaded", () => {
             request.onreadystatechange = () => {
                 if (request.readyState === 4 && request.status === 200) {
                     if (JSON.parse(request.response)["STATUS"]) {
-                        document.querySelector(".username").style.borderColor = "green"; // Turn the field borders color green
+                        document.querySelector("#u1062-4").style.borderColor = "green"; // Turn the field borders color green
                     } else {
-                        document.querySelector(".username").style.borderColor = "red"; // Turn the field borders color red
+                        document.querySelector("#u1062-4").style.borderColor = "red"; // Turn the field borders color red
                     }
                 }
             }
@@ -28,18 +28,18 @@ document.addEventListener("DOMContentLoaded", () => {
     // Used to check if the fist name are letters only
     document.querySelector(".first_name").onchange = () => {
         if (document.querySelector(".first_name").value.match(/^[A-Za-z\s]+$/)) {
-            document.querySelector(".first_name").style.borderColor = "green"; // Turn the field borders color green
+            document.querySelector("#u1074-4").style.borderColor = "green"; // Turn the field borders color green
         } else {
-            document.querySelector(".first_name").style.borderColor = "red"; // Turn the field borders color red
+            document.querySelector("#u1074-4").style.borderColor = "red"; // Turn the field borders color red
         }
     }
 
     // Used to check if the last name are letters only
     document.querySelector(".last_name").onchange = () => {
         if (document.querySelector(".last_name").value.match(/^[A-Za-z\s]+$/)) {
-            document.querySelector(".last_name").style.borderColor = "green"; // Turn the field borders color green
+            document.querySelector("#u1086-4").style.borderColor = "green"; // Turn the field borders color green
         } else {
-            document.querySelector(".last_name").style.borderColor = "red"; // Turn the field borders color red
+            document.querySelector("#u1086-4").style.borderColor = "red"; // Turn the field borders color red
         }
     }
 
@@ -54,9 +54,9 @@ document.addEventListener("DOMContentLoaded", () => {
         request.onreadystatechange = () => {
             if (request.readyState === 4 && request.status === 200) {
                 if (JSON.parse(request.response)["STATUS"]) {
-                    document.querySelector(".email").style.borderColor = "green"; // Turn the field borders color green
+                    document.querySelector("#u1098-4").style.borderColor = "green"; // Turn the field borders color green
                 } else {
-                    document.querySelector(".email").style.borderColor = "red"; // Turn the field borders color red
+                    document.querySelector("#u1098-4").style.borderColor = "red"; // Turn the field borders color red
                 }
             }
         }
@@ -65,20 +65,20 @@ document.addEventListener("DOMContentLoaded", () => {
     // Used to check password and confirm password
     document.querySelector(".pwd").onchange = () => {
         if (document.querySelector(".pwd").value.length > 7 && document.querySelector(".pwd").value.length < 65) {
-            document.querySelector(".pwd").style.borderColor = "green"; // Turn the field borders color green
+            document.querySelector("#u1110-4").style.borderColor = "green"; // Turn the field borders color green
         } else {
-            document.querySelector(".pwd").style.borderColor = "red"; // Turn the field borders color red
+            document.querySelector("#u1110-4").style.borderColor = "red"; // Turn the field borders color red
         }
     }
     document.querySelector(".cpwd").onchange = () => {
         if (document.querySelector(".cpwd").value.length > 7 && document.querySelector(".cpwd").value.length < 65 &&
         document.querySelector(".pwd").value.length > 7 && document.querySelector(".pwd").value.length < 65 &&
         document.querySelector(".pwd").value === document.querySelector(".cpwd").value) {
-            document.querySelector(".pwd").style.borderColor = "green"; // Turn the field borders color green
-            document.querySelector(".cpwd").style.borderColor = "green"; // Turn the field borders color green
+            document.querySelector("#u1110-4").style.borderColor = "green"; // Turn the field borders color green
+            document.querySelector("#u1122-4").style.borderColor = "green"; // Turn the field borders color green
         } else {
-            document.querySelector(".pwd").style.borderColor = "red"; // Turn the field borders color red
-            document.querySelector(".cpwd").style.borderColor = "red"; // Turn the field borders color red
+            document.querySelector("#u1110-4").style.borderColor = "red"; // Turn the field borders color red
+            document.querySelector("#u1122-4").style.borderColor = "red"; // Turn the field borders color red
         }
     }
 });
