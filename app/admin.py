579a52f0a6fc54d9c083bddb488fd29b529e3a43
Sana Nqoba Mngadi <sana.mngadi@gmail.com>
2020-02-22 02:32:53 +0200

    New Update. Adds YouTube iFrame to the feed of the home page. Other bugs have been smashed.

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import feedback_content, account_settings
+from .models import feedback_content, account_settings, Content
 
 # Register your models here.
 admin.site.register(feedback_content)
 admin.site.register(account_settings)
+admin.site.register(Content)
