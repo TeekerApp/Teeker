707eb4d1d9ea3e913ff43324bfeb3b815821acde
Sana Nqoba Mngadi <sana.mngadi@gmail.com>
2020-03-05 22:22:49 +0200

    Made the loading gif more appealing.

--- static/TeekerApp/js/index.js
+++ static/TeekerApp/js/index.js
@@ -1,8 +1,29 @@
 // Javascript for index page
 
+// Used for the loading gif styling to position it in the right place
+if (screen.width === 320) {
+	setTimeout(()=>{
+		document.querySelector("#loading_gif_1u").style.width = "20%";
+		document.querySelector("#loading_gif_1u").style.marginTop = "50%";
+	}, 50);
+} else if (screen.width > 320 && screen.width <= 380) {
+	setTimeout(()=>{
+		document.querySelector("#loading_gif_1u").style.width = "20%";
+		document.querySelector("#loading_gif_1u").style.marginTop = "50%";
+	}, 50);
+} else if (screen.width > 380 && screen.width <= 480) {
+	setTimeout(()=>{
+		document.querySelector("#loading_gif_1u").style.width = "20%";
+		document.querySelector("#loading_gif_1u").style.marginTop = "50%";
+	}, 50);
+}
+
+// Used to hide the content will it loads till its ready to display
 let page_completed_loading = setInterval(() => {
+	document.querySelector("#page").style.display = "None";
 	if (document.readyState === "complete") {
 		clearInterval(page_completed_loading);
 		document.querySelector("#loading_gif_1u").style.display = "None";
+		document.querySelector("#page").style.display = "inline";
 	}
 }, 100);
