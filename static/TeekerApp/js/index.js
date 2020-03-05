9c792f4f9a0f5859c9b7f3517b62e104cea78737
Sana Nqoba Mngadi <sana.mngadi@gmail.com>
2020-03-05 22:33:37 +0200

    Made the loading gif more appealing.

--- static/TeekerApp/js/index.js
+++ static/TeekerApp/js/index.js
@@ -1,20 +1,25 @@
 // Javascript for index page
 
+var screen_width = (screen.width / 2) - 15;
+alert(screen_width);
 // Used for the loading gif styling to position it in the right place
 if (screen.width === 320) {
 	setTimeout(()=>{
 		document.querySelector("#loading_gif_1u").style.width = "20%";
 		document.querySelector("#loading_gif_1u").style.marginTop = "50%";
+		document.querySelector("#loading_gif_1u").style.marginLeft = screen_width+"px";
 	}, 50);
 } else if (screen.width > 320 && screen.width <= 380) {
 	setTimeout(()=>{
 		document.querySelector("#loading_gif_1u").style.width = "20%";
 		document.querySelector("#loading_gif_1u").style.marginTop = "50%";
+		document.querySelector("#loading_gif_1u").style.marginLeft = screen_width+"px";
 	}, 50);
 } else if (screen.width > 380 && screen.width <= 480) {
 	setTimeout(()=>{
 		document.querySelector("#loading_gif_1u").style.width = "20%";
 		document.querySelector("#loading_gif_1u").style.marginTop = "50%";
+		document.querySelector("#loading_gif_1u").style.marginLeft = screen_width+"px";
 	}, 50);
 }
 
